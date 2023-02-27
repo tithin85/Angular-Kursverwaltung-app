e6e83441379cba855079b7235a781e6e96c8dff2
ozge <othiel@uni-bremen.de>
2023-02-27 11:50:27 +0100

    home Components

--- src/app/app-routing.module..ts
+++ src/app/app-routing.module..ts
@@ -3,7 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import {PersonListComponent} from "./person-list/person-list.component";
 import {KursListComponent} from "./kurs-list/kurs-list.component";
 import {PersonFormularComponent} from "./person-formular/person-formular.component";
+import {HomeComponent} from "./home/home.component";
 const routes: Routes = [ {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
   path: 'PersonList',
   component:PersonListComponent
 },
@@ -11,6 +21,7 @@ const routes: Routes = [ {
     path: 'KursList',
     component:KursListComponent
   },
+
   {
     path: 'PersonForm',
     component: PersonFormularComponent
