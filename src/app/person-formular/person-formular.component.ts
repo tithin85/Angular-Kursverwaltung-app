0076fb1cb23af2de38933ca4ce18894aee03f3b4
Tithin <ttomy@uni-bremen.de>
2023-02-27 11:53:19 +0100

    post method functioniert

--- src/app/person-formular/person-formular.component.ts
+++ src/app/person-formular/person-formular.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Person} from "../shared/Person";
+import {PersonStoreService} from "../shared/person-store.service";
 
 @Component({
   selector: 'app-person-formular',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./person-formular.component.css']
 })
 export class PersonFormularComponent {
+  person:Person;
+  return:any
+  constructor(private service:PersonStoreService) {
+    this.person={
+      "titel":"",
+      "name":"",
+      "vorname":"",
+      "strasse":"",
+      "plz":"",
+      "ort":"",
+      "email":""
+
+    }
+
+  }
+  ngOnInit(){
+
+  }
+  anmeldung(){
+    let response=this.service.addPerson(this.person);
+    response.subscribe((data)=>this.return=data)
+  }
+
 
 }
