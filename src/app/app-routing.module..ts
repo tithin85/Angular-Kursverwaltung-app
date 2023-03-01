418418ec55034b1742ec405ac1cc42ee8aca6b05
th62 <haberland@webtaktik.de>
2023-03-01 19:31:33 +0100

    Vorbereitung fuer Security-login

--- src/app/app-routing.module..ts
+++ src/app/app-routing.module..ts
@@ -1,42 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {PersonListComponent} from "./person-list/person-list.component";
 import {KursListComponent} from "./kurs-list/kurs-list.component";
 import {PersonFormularComponent} from "./person-formular/person-formular.component";
 import {HomeComponent} from "./home/home.component";
 import {KursFormComponent} from "./kurs-form/kurs-form.component";
-const routes: Routes = [ {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
 
-  path: 'PersonList',
-  component:PersonListComponent
-},
-  {
-    path: 'KursList',
-    component:KursListComponent
-  },
-
-  {
-    path: 'PersonForm',
-    component: PersonFormularComponent
-  },
-  {
-    path: 'KursForm',
-    component: KursFormComponent
-  }
-
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'PersonList', component: PersonListComponent },
+  { path: 'KursList', component: KursListComponent },
+  { path: 'PersonForm', component: PersonFormularComponent },
+  { path: 'KursForm', component: KursFormComponent }
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
 
