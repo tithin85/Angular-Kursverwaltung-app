f15f7b8545cc0d41286505e8d22bc9312054bbb8
th62 <haberland@webtaktik.de>
2023-03-09 17:20:53 +0100

    componenten für öffentliche kursliste und details vorbereitet

--- src/app/app-routing.module..ts
+++ src/app/app-routing.module..ts
@@ -2,6 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import { AuthGuard } from './_services/auth.guard';
 
+import {UserLoginComponent} from "./user-login/user-login.component";
+import {UserRegisterComponent} from "./user-register/user-register.component";
+
+import {PageNotFoundComponentComponent} from "./page-not-found-component/page-not-found-component.component";
+
 import {HomeComponent} from "./home/home.component";
 import {HomeKurslistComponent} from "./home-kurslist/home-kurslist.component";
 import {HomeKursdetailsComponent} from "./home-kursdetails/home-kursdetails.component";
@@ -11,10 +16,7 @@ import {PersonListComponent} from "./person-list/person-list.component";
 import {PersonDetailsComponent} from "./person-details/person-details.component";
 import {KursFormComponent} from "./kurs-form/kurs-form.component";
 import {KursListComponent} from "./kurs-list/kurs-list.component";
-import {UserLoginComponent} from "./user-login/user-login.component";
-import {UserRegisterComponent} from "./user-register/user-register.component";
 import {PersonZuordnungComponent} from "./person-zuordnung/person-zuordnung.component";
-import {PageNotFoundComponentComponent} from "./page-not-found-component/page-not-found-component.component";
 import {KursDetailsComponent} from "./kurs-details/kurs-details.component";
 import {KursZuordnungComponent} from "./kurs-zuordnung/kurs-zuordnung.component";
 
@@ -24,15 +26,18 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'homekurslist', component: HomeKurslistComponent },
   { path: 'homekursdetails', component: HomeKursdetailsComponent },
+
+  { path: 'login', component: UserLoginComponent },
+  { path: 'register', component: UserRegisterComponent },
+
   { path: 'personform', component: PersonFormComponent, canActivate: [AuthGuard] },
   { path: 'personlist', component: PersonListComponent, canActivate: [AuthGuard] },
   { path: 'persondetails', component: PersonDetailsComponent, canActivate: [AuthGuard] },
+  { path: 'zuordnung/person/:personId', component: PersonZuordnungComponent, canActivate: [AuthGuard] },
+
   { path: 'kursform', component: KursFormComponent, canActivate: [AuthGuard] },
   { path: 'kurslist', component: KursListComponent, canActivate: [AuthGuard] },
   { path: 'kursdetails', component: KursDetailsComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: UserLoginComponent },
-  { path: 'register', component: UserRegisterComponent },
-  { path: 'zuordnung/person/:personId', component: PersonZuordnungComponent, canActivate: [AuthGuard] },
   { path: 'zuordnung/kurs/:kursId', component: KursZuordnungComponent, canActivate: [AuthGuard] },
 
   { path: '', redirectTo: 'home', pathMatch: 'full' },
